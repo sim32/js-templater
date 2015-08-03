aeb61320a7ca9ce1eaaab9ac3513907c4ad69b6b
sim <sim7c6@gmail.com>
2015-08-03 12:06:24 +0600

    add comments

--- tpl-init.js
+++ tpl-init.js
@@ -1,26 +1,26 @@
 /*first initial template*/
-_.onTemplateReady('firstTemplate', function(callback, attribs) {
+_.onTemplateReady('firstTemplate', function (callback, attribs) {
     //nothing happens because firstTemplate has attrib renderOnRequest equals 1
     callback({count: 9});
 });
-_.onTemplateReady('secondTemplate', function(callback, attribs) {
+_.onTemplateReady('secondTemplate', function (callback, attribs) {
     //now secondTemplate will be rendered
     callback({count: 9});
 });
 /*if need reinitial tpl you must usage reRenderContaintTemplate with tamplate name at first param and
-* function with any logic on second param. At the end of function callback
-* */
-$(document).on('click', function() {
-    _.reRenderSingleTemplate('singleTemplate', function(callback) {
+ * function with any logic on second param. At the end of function callback
+ * */
+$(document).on('click', function () {
+    _.reRenderSingleTemplate('singleTemplate', function (callback) {
         //singleTemplate missing on page. nothing happens.
         var max = 7, min = 0;
         var cnt = Math.floor(Math.random() * (max - min + 1)) + min;
         callback({count: cnt});
     })
 
-    if(!_.isRendered('firstTemplate')) {
+    if (!_.isRendered('firstTemplate')) {
         _.removeFromPage('secondTemplate'); //remove secondTemplate
-        _.renderSingleTemplate('firstTemplate', function(callback) { //render firstTemplate
+        _.renderSingleTemplate('firstTemplate', function (callback) { //render firstTemplate
             callback({count: 3});
         })
     }
--- underscore-mix.js
+++ underscore-mix.js
@@ -1,51 +1,40 @@
-(function(_, $){
+(function (_, $) {
     /*надстройка над шаблонизатором underscore*/
     var templatesEvents = [];
-    var renderedView  = [];
+    var renderedView = [];
 
     _.templateSettings.variable = "fv"; // front variables
 
     _.mixin({
 
         /*на входе имя шаблона и колбэк*/
-        onTemplateReady: function(name, callback) {
-            if(!_.isString(name) | !_.isFunction(callback)) {
+        onTemplateReady: function (name, callback) {
+            if (!_.isString(name) | !_.isFunction(callback)) {
                 return false;
             }
 
-            templatesEvents.push({element: name, callback: callback });
+            templatesEvents.push({element: name, callback: callback});
         },
 
-        /*на входе имя шаблона*/
-        destroyEvents: function(element) {
-            if(!_.isString(element)) {
-                return false;
-            }
-            templatesEvents = _.reject(templatesEvents, function(event) {
-                if(event.element != element) return true;
-            })
-        },
-
-        reRenderSingleTemplate: function(name, callback) {
+        reRenderSingleTemplate: function (name, callback) {
             var id = _.getIdByTplName(name);
 
-            if(!_.isUndefined(id)) {
+            if (!_.isUndefined(id)) {
                 var templateText = _.findWhere(renderedView, {id: id}).templateText;
-                var deprElement = $('[template-id='+id+']');
-                if(deprElement.length) {
+                var deprElement = $('[template-id=' + id + ']');
+                if (deprElement.length) {
 
-                    var renderer = _.partial(function(replacement, result) {
+                    var renderer = _.partial(function (replacement, result) {
                         var func = _.template(templateText);
 
-                        renderedView = _.reject(renderedView, function(elem) {
-                            if(id == elem.id) return true;
+                        renderedView = _.reject(renderedView, function (elem) {
+                            if (id == elem.id) return true;
                         });
 
-                        var html = func( result );
-                        id =  _.uniqueId('tpl_');
+                        var html = func(result);
+                        id = _.uniqueId('tpl_');
 
-                        $(replacement).replaceWith($('<container>'+html+'</container>').attr('template-id', id));
-
+                        $(replacement).replaceWith($('<container>' + html + '</container>').attr('template-id', id));
 
 
                         renderedView.push({
@@ -62,21 +51,21 @@
             }
         },
 
-        renderSingleTemplate: function(name, callback){
+        renderSingleTemplate: function (name, callback) {
             var id = _.getIdByTplName(name);
-            if(!_.isUndefined(id)) {
+            if (!_.isUndefined(id)) {
                 reRenderSingleTemplate(name, callback);
             } else {
-                var element = $(".template[name="+name+"]");
-                if($(element).length) {
+                var element = $(".template[name=" + name + "]");
+                if ($(element).length) {
                     var templateText = $(element).text();
-                    var renderer = _.partial(function(element, result) {
+                    var renderer = _.partial(function (element, result) {
                         var func = _.template(templateText);
 
-                        var html = func( result );
-                        var id =  _.uniqueId('tpl_');
+                        var html = func(result);
+                        var id = _.uniqueId('tpl_');
 
-                        $(element).replaceWith($('<container>'+html+'</container>').attr('template-id', id));
+                        $(element).replaceWith($('<container>' + html + '</container>').attr('template-id', id));
 
                         renderedView.push({
                             name: name,
@@ -88,10 +77,10 @@
 
                     var attributes = {};
 
-                    $(element.attributes).each(function() {
+                    $(element.attributes).each(function () {
                         var match = this.nodeName.match(/tpl-data(-(\w+))/);
-                        if(!_.isNull(match)) {
-                            attributes[ match[2] ] = $(element).attr('tpl-data-'+match[2]);
+                        if (!_.isNull(match)) {
+                            attributes[match[2]] = $(element).attr('tpl-data-' + match[2]);
                         }
                     });
 
@@ -100,26 +89,26 @@
             }
         },
 
-        renderContaintTemplate: function() {
-            $( document ).ready( function() {
-                $('.template').each(function(index, val) {
-                    if(!_.isUndefined($(val).attr('renderOnRequest')) & $(val).attr('renderOnRequest') == 1) {
+        renderContaintTemplate: function () {
+            $(document).ready(function () {
+                $('.template').each(function (index, val) {
+                    if (!_.isUndefined($(val).attr('renderOnRequest')) & $(val).attr('renderOnRequest') == 1) {
                         return;
                     }
                     var templateText = $(val).text();
-                    var attrs = _.where(templatesEvents, {element: $(val).attr('name') });
+                    var attrs = _.where(templatesEvents, {element: $(val).attr('name')});
 
 
-                    if(_.size(attrs) > 0) {
-                        _.each(attrs, function(element, index, list) {
+                    if (_.size(attrs) > 0) {
+                        _.each(attrs, function (element, index, list) {
 
-                            var renderer = _.partial(function(val, result) {
+                            var renderer = _.partial(function (val, result) {
                                 var func = _.template(templateText);
 
-                                var html = func( result );
-                                var id =  _.uniqueId('tpl_');
+                                var html = func(result);
+                                var id = _.uniqueId('tpl_');
 
-                                $(val).replaceWith($('<container>'+html+'</container>').attr('template-id', id));
+                                $(val).replaceWith($('<container>' + html + '</container>').attr('template-id', id));
 
                                 renderedView.push({
                                     name: element.element,
@@ -131,10 +120,10 @@
 
                             var attributes = {};
 
-                            $(val.attributes).each(function() {
+                            $(val.attributes).each(function () {
                                 var match = this.nodeName.match(/tpl-data(-(\w+))/);
-                                if(!_.isNull(match)) {
-                                    attributes[ match[2] ] = $(val).attr('tpl-data-'+match[2]);
+                                if (!_.isNull(match)) {
+                                    attributes[match[2]] = $(val).attr('tpl-data-' + match[2]);
                                 }
                             });
                             element.callback(renderer, attributes);
@@ -142,29 +131,29 @@
                         })
                     }
                 })
-            } )
+            })
         },
-        getIdByTplName: function(name) {
+        getIdByTplName: function (name) {
             var element = _.findWhere(renderedView, {name: name});
-            if(_.isUndefined(element) ) {
+            if (_.isUndefined(element)) {
                 return element;
             }
             return element.id;
         },
 
-        isRendered: function(name) {
+        isRendered: function (name) {
             var id = _.getIdByTplName(name);
-            if(_.isUndefined(id)) return false;
+            if (_.isUndefined(id)) return false;
 
-            return $('[template-id='+id+']').length == 1;
+            return $('[template-id=' + id + ']').length == 1;
         },
 
-        removeFromPage: function(name) {
+        removeFromPage: function (name) {
             var id = _.getIdByTplName(name);
-            if(!_.isUndefined(id)) {
-                $('[template-id='+id+']').remove();
-                renderedView = _.reject(renderedView, function(elem) {
-                    if(name == elem.name) return true;
+            if (!_.isUndefined(id)) {
+                $('[template-id=' + id + ']').remove();
+                renderedView = _.reject(renderedView, function (elem) {
+                    return name == elem.name;
                 })
             }
         }
